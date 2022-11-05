f967cf796e4ee0ac47e1a3a77610af91d79a4d78
Leonardo Meyer <leonardo.meyer@alpar.com.br>
2022-11-04 21:24:27 -0300

    Inclusão de arquivos iniciais

--- bin/global.js
+++ bin/global.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 // Delete the 0 and 1 argument (node and script.js)
-var args = process.argv.splice(process.execArgv.length + 2);
+const args = process.argv.splice(process.execArgv.length + 2);
 
 // Retrieve the first argument
-var name = args[0];
+const name = args[0];
 
-var myLibrary = require('../src/index.js');
+const myLibrary = require("../lib/index.js");
 
 // Displays the text in the console
-myLibrary.say(name + ', get back, come on before we crack Lose your blues, everybody cut footloose');
+const dirs = process.cwd().split("\\");
+const repoName = dirs[dirs.length - 1];
+myLibrary.say(name + " | " + repoName);
